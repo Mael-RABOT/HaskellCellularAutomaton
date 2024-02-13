94df2b596808e07cf607481fdecf41a25511bdde
Mael-RABOT <mael.rabot@gmail.com>
2024-02-13 16:36:57 +0100

    fix(getOpt): it seem this is forbiden
    Signed-off-by: Mael-RABOT <mael.rabot@gmail.com>

--- src/Lib.hs
+++ src/Lib.hs
@@ -11,7 +11,6 @@ module Lib
     , applyRuleNTimes
     ) where
 
-import System.Console.GetOpt
 import Text.Read (readMaybe)
 
 data Conf = Conf (Maybe Int) (Maybe Int) (Maybe Int) (Maybe Int) (Maybe Int)
@@ -20,31 +19,16 @@ data Conf = Conf (Maybe Int) (Maybe Int) (Maybe Int) (Maybe Int) (Maybe Int)
 defaultConf :: Conf
 defaultConf = Conf Nothing (Just 0) Nothing (Just 80) (Just 0)
 
-data Flag = Rule String | Start String | Lines String | Window String | Move String
-
-options :: [OptDescr Flag]
-options =
-    [ Option [] ["rule"] (ReqArg Rule "") "Option rule"
-    , Option [] ["start"] (ReqArg Start "") "Option start"
-    , Option [] ["lines"] (ReqArg Lines "") "Option lines"
-    , Option [] ["window"] (ReqArg Window "") "Option window"
-    , Option [] ["move"] (ReqArg Move "") "Option move"
-    ]
-
 parseOptions :: [String] -> Maybe Conf
-parseOptions argv =
-    case getOpt Permute options argv of
-        (flags, _, []) -> Just $ foldl (flip setFlag) defaultConf flags
-        (flags, _, errs) -> if not (null errs)
-                        then Nothing
-                        else Just $ foldl (flip setFlag) defaultConf flags
-
-setFlag :: Flag -> Conf -> Conf
-setFlag (Rule arg) (Conf _ b c d e) = Conf (readMaybe arg) b c d e
-setFlag (Start arg) (Conf a _ c d e) = Conf a (readMaybe arg) c d e
-setFlag (Lines arg) (Conf a b _ d e) = Conf a b (readMaybe arg) d e
-setFlag (Window arg) (Conf a b c _ e) = Conf a b c (readMaybe arg) e
-setFlag (Move arg) (Conf a b c d _) = Conf a b c d (readMaybe arg)
+parseOptions argv = f defaultConf argv
+  where
+    f conf [] = Just conf
+    f (Conf _ b c d e) ("-rule":arg:rest) = f (Conf (readMaybe arg) b c d e) rest
+    f (Conf a _ c d e) ("-start":arg:rest) = f (Conf a (readMaybe arg) c d e) rest
+    f (Conf a b _ d e) ("-lines":arg:rest) = f (Conf a b (readMaybe arg) d e) rest
+    f (Conf a b c _ e) ("-window":arg:rest) = f (Conf a b c (readMaybe arg) e) rest
+    f (Conf a b c d _) ("-move":arg:rest) = f (Conf a b c d (readMaybe arg)) rest
+    f _ _ = Nothing
 
 checkArgs :: Conf -> Maybe Conf
 checkArgs (Conf (Just rule) start line window move)

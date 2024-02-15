46a13f12646adbf415779862f9a2039f4c9f5d60
Mael-RABOT <mael.rabot@gmail.com>
2024-02-15 15:41:01 +0100

    fix(display): Fix the number of line displayed
    Signed-off-by: Mael-RABOT <mael.rabot@gmail.com>

--- src/Lib.hs
+++ src/Lib.hs
@@ -23,7 +23,9 @@ f :: Conf -> [String] -> Maybe Conf
 f conf [] = Just conf
 f (Conf _ b c d e) ("-rule":arg:rest) =f (Conf (readMaybe arg) b c d e) rest
 f (Conf a _ c d e) ("-start":arg:rest) = f (Conf a (readMaybe arg) c d e) rest
-f (Conf a b _ d e) ("-lines":arg:rest) = f (Conf a b (readMaybe arg) d e) rest
+f (Conf a b _ d e) ("-lines":arg:rest) =
+    let newLines = fmap (\x -> x - 1) (readMaybe arg)
+    in f (Conf a b newLines d e) rest
 f (Conf a b c _ e) ("-window":arg:rest) = f (Conf a b c (readMaybe arg) e) rest
 f (Conf a b c d _) ("-move":arg:rest) = f (Conf a b c d (readMaybe arg)) rest
 f _ _ = Nothing
